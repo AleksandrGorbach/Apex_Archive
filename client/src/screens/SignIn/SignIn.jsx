63b3f13b28250ca04e0d6d9357de3ba7bd11b002
aleksandr gorbach <alex@aleksandrs-MacBook-Air.local>
2021-08-27 19:54:03 -0400

    signin complete

--- client/src/screens/SignIn/SignIn.jsx
+++ client/src/screens/SignIn/SignIn.jsx
@@ -22,6 +22,30 @@ export default function Login(props) {
                 e.preventDefault();
                 handleLogin(formData);
             }}
-        ></form>
-    )
+        >
+            <h3>Login</h3>
+            <label>
+                Username:
+                <input
+                    type='text'
+                    name='username'
+                    value={formData.username}
+                    onChange={handleChange}
+                />
+            </label>
+            <br />
+            <label>
+                Password:
+                <input
+                type='password'
+                name='password'
+                value={formData.password}
+                onChange={handleChange}
+                />
+            </label>
+            <br />
+            <Link to='/register'>Register</Link>
+            <button>Submit</button>
+        </form>
+    );
 }
