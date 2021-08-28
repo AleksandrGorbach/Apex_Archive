5c37d89341cb6baf2e22347b78b214eea86ed85d
aleksandr gorbach <alex@aleksandrs-MacBook-Air.local>
2021-08-27 20:24:40 -0400

    layout

---
+++ client/src/components/Layout/Layout.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+export default function Layout(props) {
+    const { currentUser, handleLogout } = props;
+    return (
+        <header>
+            <h1>Apex Archive</h1>
+            {currentUser ? (
+                <div>
+                    <p>{currentUser.username}</p>
+                    <button onClick={handleLogout}>Logout</button>
+                </div>
+            ) : (
+                <Link to='/login'>Login/Register</Link>
+            )}
+            <hr />
+            {currentUser && (
+                <div>
+                    <Link to='/articles'>Articles</Link>
+                </div>
+            )}
+            {props.children}
+        </header>
+    );
+}
