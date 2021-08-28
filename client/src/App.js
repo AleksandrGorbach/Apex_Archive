7d2fac4cf0c4c5f7948f3327fc858c5312a96855
aleksandr gorbach <alex@aleksandrs-MacBook-Air.local>
2021-08-27 20:32:33 -0400

    handle login and register

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { Switch, Route, useHistory } from 'react-router-dom';
 import './App.css';
+import Layout from './components/Layout/Layout';
+import SignIn from './screens/SignIn/SignIn';
+import SignUp from './screens/SignUp/SignUp';
+import {
+  loginUser,
+  registerUser,
+  verifyUser,
+  removeToken,
+} from './services/auth';
+
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  const histroy = useHistory();
+
+  useEffect(() => {
+    const handleVerify = async () => {
+      const userData = await verifyUser();
+      setCurrentUser(userData);
+    };
+    handleVerify();
+  }, []);
+
+  const handleLogin = async (loginData) => {
+    const userData = await longinUser(loginData);
+    setCurrentUser(userData);
+    history.push('/');
+  };
+
+  const handleRegister = async (registerData) => {
+    const userData = await registerUser(registerData);
+    setCurrentUser(userData);
+    history.push('/');
+  };
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
