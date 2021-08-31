6ce38a3aa91256c3630435f58316a51335ea2956
aleksandr gorbach <alex@aleksandrs-MacBook-Air.local>
2021-08-31 14:25:01 -0400

    aos added to card

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import AOS from "aos";
+import "aos/dist/aos.css";
 import { Link } from 'react-router-dom';
 
 export default function Card({article}) {
     return (
-        <div className="flex justify-between m-6">
+        <div data-aos="zoom-in-up" data-aos-duration="1000" className="flex justify-between m-6">
             <div className="flex flex-col min-h-100 w-72 border-2 mx-auto bg-white p-2 rounded-lg shadow-lg">
                 <Link to={`/articles/${article.id}`}>
                 <img src={article.img_url} alt={article.title} className="rounded flex flex-col justify-center h-500 w-auto bg-cover"/>                                
