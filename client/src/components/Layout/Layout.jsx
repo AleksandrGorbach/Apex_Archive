21d8e5fb3f9267ce420aae64cfa67cfccf2fe14e
aleksandr gorbach <alex@aleksandrs-MacBook-Air.local>
2021-08-31 21:35:24 -0400

    navbar changes and uppercase function

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -4,26 +4,10 @@ import Footer from '../Footer/Footer';
 import Navbar from '../Navbar/Navbar';
 
 export default function Layout(props) {
-    // const { currentUser, handleLogout } = props;
+    const { currentUser, handleLogout } = props;
     return (
-        <header>
-            <Navbar />
-            {/* <h1>Apex Archive</h1>
-            {currentUser ? (
-                <div>
-                    <p>{currentUser.username}</p>
-                    <button onClick={handleLogout}>Logout</button>
-                </div>
-            ) : (
-                <Link to='/login'>Login/Register</Link>
-            )}
-            <hr />
-            {currentUser && (
-                <div>
-                    <Link to='/articles'>Articles</Link>
-                    <Link to='/articles/new'>Post Article</Link>
-                </div>
-            )} */}
+        <header className="bg-pallette-backgroundLight w-full h-full">
+            <Navbar currentUser={currentUser} handleLogout={handleLogout}/>
             {props.children}
             <Footer />
         </header>
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -4,26 +4,33 @@ import { Link } from 'react-router-dom'
 export default function Navbar(props) {
 const { currentUser, handleLogout } = props;
 
-    return (
-        <div>
-            <Link to='/articles'><h1>Apex Archive</h1></Link>
-            <Link to='/articles/new'><h1>New Post</h1></Link>
-            {currentUser ? (
-                <div>
-                    <p>{currentUser.username}</p>
-                    <button onClick={handleLogout}>Logout</button>
-                </div>
-            ) : (
-                <Link to='/login'>Login/Register</Link>
-            )}
-            <hr />
-            {currentUser && (
-                <div>
-                    <Link to='/articles'>Articles</Link>
-                    <Link to='/articles/new'>Post Article</Link>
-                </div>
-            )}
-            {props.children}
-        </div>
-    )
+const upperCase = (str) => {
+    return str.replace(/\w\S*/g, function(txt){
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    })
 }
+    return (
+          <div className='z-10 bg-palette-darkCard shadow-lg'>
+      <nav className='flex xl:flex-row xl:justify-evenly xl:flex-nowrap sm:align-baseline'>
+        <Link className='mr-1 text-palette-font text-2xl' to='/articles'>Apex Archive</Link>
+        <Link to='/articles/new' className='ml-1 text-palette-font'>New Post</Link>
+      {currentUser ? (
+        <div className='flex text-palette-font'>
+          <Link to={`/profile/${currentUser.username}`} className="mr-5 "><p className="mr-1">Hi, {upperCase(currentUser.username)}</p></Link>
+          <button className="border ml-6" onClick={handleLogout}>Logout</button>
+        </div>
+        ) : (
+            <div className="mr-1">
+              <Link to='/login'>Login/Register</Link>
+            </div>
+        )}
+        {currentUser && (
+          <div>
+            <Link to='/articles/new' className="ml-1 text-palette-font" >Create Article</Link>
+          </div>
+        )}
+          </nav>
+          {props.children}
+    </div>
+    )}
+
