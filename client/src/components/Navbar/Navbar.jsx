f0e43f8134470563984fd6cde922ae0672ce0e38
aleksandr gorbach <alex@aleksandrs-MacBook-Air.local>
2021-09-01 03:26:17 -0400

    new changes

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -13,9 +13,9 @@ const upperCase = (str) => {
     <div className='z-10 bg-palette-darkCard bg-gray-900 shadow-lg'>
       <nav className='flex xl:flex-row xl:justify-evenly xl:flex-nowrap sm:align-baseline'>
         <Link className='mr-1 text-palette-font text-2xl' to='/articles'>Apex Archive</Link>
-        <Link to='/articles/new' className='ml-1 text-palette-font'>New Post</Link>
+        <Link to='/articles/new' className='mr-56 text-palette-font text-2xl'>New Post</Link>
       {currentUser ? (
-        <div className='flex text-palette-font'>
+        <div className='flex text-palette-font mr-1 text-2xl'>
           <Link to={`/profile/${currentUser.username}`} className="mr-5 "><p className="mr-1">Hi, {upperCase(currentUser.username)}</p></Link>
           <button className="border ml-44 bg-firstgreen hover:bg-secondgreen text-black py-1 px-1 rounded-lg" onClick={handleLogout}>Logout</button>
         </div>
@@ -26,7 +26,7 @@ const upperCase = (str) => {
         )}
         {currentUser && (
           <div>
-            <Link to='/articles/new' className="ml-1 text-palette-font" >Create Article</Link>
+            <Link to='/articles/new' className="mr-1 text-palette-font text-2xl" >Create Article</Link>
           </div>
         )}
           </nav>
