1c6e0ab94dbdc7c4df4b7c09f13c4ea04c9128b0
aleksandr gorbach <alex@aleksandrs-MacBook-Air.local>
2021-09-01 01:59:01 -0400

    slider data

---
+++ client/src/components/ImageSlider/SliderData.jsx
@@ -0,0 +1,18 @@
+export const sliderData = [
+  {
+    name:
+      "No posting fees for any inquiries! Feel free to ask whatever services you need!",
+    image:
+      "https://www.agwilliamspainting.com/wp-content/uploads/2018/07/interior-house-painting-new-york-and-connecticut.png",
+  },
+  {
+    name: "You know perfectly well the importance of hiring the right person for the job.",
+    image: "https://miro.medium.com/max/3398/1*JEKxItWOdONjJA-4RGVtMg.jpeg",
+  },
+  {
+    name:
+      "If you're looking for any service provider near you, always visit SkillBid!",
+    image:
+      "https://biltwellroofing.com/wp-content/uploads/2017/11/Replacement.jpg",
+  },
+];
