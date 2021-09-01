e9e81b4a4253bf991c33f764cb4fa2a908a76e80
aleksandr gorbach <alex@aleksandrs-MacBook-Air.local>
2021-09-01 00:25:42 -0400

    detail page styling addition

--- client/src/screens/ArticleDetail/ArticleDetail.jsx
+++ client/src/screens/ArticleDetail/ArticleDetail.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { getOneArticle } from '../../services/articles';
-
+import './ArticleDetail.css'
 
 export default function ArticleDetail(props) {
   const [articleItem, setArticleItem] = useState({});
@@ -20,9 +20,9 @@ export default function ArticleDetail(props) {
 
   return (
     <div className="detail-container">
-      <img src={articleItem.img_url}/>
-      <h3>{articleItem?.title}</h3>
-      <p>{articleItem?.content}</p>    
+      <img className="detail-img" src={articleItem.img_url}/>
+      <h1 className="detail-title">{articleItem?.title}</h1>
+      <p className="detail-content">{articleItem?.content}</p>    
           {currentUser?.id === articleItem?.user_id && (
             <div>
               <Link to={`/articles/${articleItem?.id}/edit`}>
