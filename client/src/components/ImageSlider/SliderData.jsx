0a63323151d06198c45506ff6f66e6d06b89391e
aleksandr gorbach <alex@aleksandrs-MacBook-Air.local>
2021-09-01 02:14:19 -0400

    slider implemented

--- client/src/components/ImageSlider/SliderData.jsx
+++ client/src/components/ImageSlider/SliderData.jsx
@@ -1,17 +1,12 @@
 export const sliderData = [
   {
-    name:
-      "No posting fees for any inquiries! Feel free to ask whatever services you need!",
     image:
       "https://wallpapercave.com/wp/wp5400337.png",
   },
   {
-    name: "You know perfectly well the importance of hiring the right person for the job.",
     image: "https://images.hdqwalls.com/wallpapers/4k-apex-legends-2020-ja.jpg",
   },
   {
-    name:
-      "If you're looking for any service provider near you, always visit SkillBid!",
     image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC67o3nRi4f-cjwEF3nnhmscNBWHBZVv3opA&usqp=CAU",
   },
--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import Footer from '../Footer/Footer';
 import Navbar from '../Navbar/Navbar';
+import ImageSlider from '../ImageSlider/ImageSlider';
 
 export default function Layout(props) {
     const { currentUser, handleLogout } = props;
     return (
         <header className="bg-pallette-backgroundLight w-full h-full">
             <Navbar currentUser={currentUser} handleLogout={handleLogout}/>
+            <ImageSlider />
             {props.children}
             <Footer />
         </header>
